569665924b1bd6e8a8a19bbe8b9e9a6b60fd07a1
maabfw <maabfw@proton.me>
2024-02-03 16:50:54 +0600

    1task done

---
+++ 1task.js
@@ -0,0 +1,10 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function fourMultiply(a, b, c, d) {
+    const result = a * b * c * d
+    return result
+}
+
+
+result = fourMultiply(6, 5, 9, 8)
+console.log(result);
